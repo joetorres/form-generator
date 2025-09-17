050ad5c68f9fd74024ed90fa99472dd8210929fc
Jerônimo Torres <jeronimo.torres@outlook.com>
2025-09-16 22:56:15 -0300

    Initial Commit

---
+++ form_generator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  form_generator
+//
+//  Created by Jerônimo Torres on 16/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ form_generator/form_generatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  form_generatorApp.swift
+//  form_generator
+//
+//  Created by Jerônimo Torres on 16/09/25.
+//
+
+import SwiftUI
+
+@main
+struct form_generatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
